d704c7f1a3017ddeddcfadd80fdd162b6e12a1ad
rpanteleychuk <r.panteleychuk@gmail.com>
2021-08-10 10:23:19 +0300

    start theme

--- wp-content/themes/ovramko-theme/template-parts/content-page.php
+++ wp-content/themes/ovramko-theme/template-parts/content-page.php
@@ -1,6 +1,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+<!--	--><?php //the_title( '<h1 class="page-title">', '</h1>' ); ?>
 
 	<div class="page-content">
 		<?php the_content(); ?>
